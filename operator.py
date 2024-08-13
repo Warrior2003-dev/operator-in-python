e29eaecca03dfefd3b5d8b96925afb66bece38b3
Jyotii <suryaprajapati29@gmail.com>
2024-08-13 22:02:40 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,65 @@
+#first program
+print("this is my first program")
+
+#using operator
+#1.arithmetic operator
+print("arithmetic operator")
+a=20
+b=10
+print(a+b)             #addition
+print(a-b)             #subtraction
+print(a*b)             #multiplication
+print(a/b)             #division
+print(a%b)            #modulus
+
+#comparison operator
+print("comparison operator")
+a=35
+b=65
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+
+#logical operator
+print("logical operator")
+a=23
+b=12
+print(a and b)
+print(a or b)
+print(a ,not b)
+
+#bitwise operator
+print("bitwise operator")
+a=34
+b=60
+print(a&b)
+print(a|b)
+print(a^b)
+print(a,~b)
+print(a<<b)
+print(a>>b)
+
+# membership  operator")
+
+print("membership  operator")
+a=("happy independence day")
+
+print("day"in a)
+print("day"not in a)
+
+#identity operator
+print("identity operator")
+x=["eye,nose,tongue,ear"]
+y=["apple,banana,grapes"]
+z=x
+print(x is z)
+print(x is y)
+print(x is not y)
+
+
+
+
+
+
+
